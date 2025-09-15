1b0fa685250e7f454a7072e44ca9b7b8186cdc78
Ana Cremasco <anaclara.luizcremasco@gmail.com>
2025-09-15 15:59:02 -0300

    feat: cria as queries de select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,19 @@ CREATE TABLE artistas (
     musica_famosa VARCHAR(50),
     ativo BOOLEAN
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+('Adele', 'Solo', 1, 'Pop', 2006, 'Reino Unido', 'Rolling in the Deep', TRUE),
+('21 Pilots', 'Banda', 2, 'Rock Alternativo', 2005, 'Estados Unidos', 'Stressed Out', TRUE),
+('BTS', 'Grupo', 7, 'K-Pop', 2013, 'Coréia do Sul', 'Dynamite', TRUE)
+('Queen', 'Banda', 4, 'Rock', 1970, 'Reino Unido', 'Bohemian Rhapsody', FALSE),
+('Taylor Swift', 'Solo', 1, 'Pop', 2004, 'Estados Unidos', 'Shake It Off', TRUE),
+('The Beatles', 'Banda', 4, 'Rock', 1960, 'Reino Unido', 'Hey Jude', FALSE),
+('Coldplay', 'Banda', 4, 'Rock Alternativo', 1996, 'Reino Unido', 'Yellow', TRUE),
+('Eminem', 'Solo', 1, 'Hip Hop', 1996, 'Estados Unidos', 'Lose Yourself', TRUE),
+('AC/DC', 'Banda', 5, 'Hard Rock', 1973, 'Austrália', 'Highway to Hell', TRUE),
+('Linkin Park', 'Banda', 6, 'Nu Metal', 1996, 'Estados Unidos', 'In the End', FALSE),
+('Rihanna', 'Solo', 1, 'R&B', 2005, 'Barbados', 'Umbrella', TRUE),
+('Daft Punk', 'Dupla', 2, 'Eletrônica', 1993, 'França', 'Get Lucky', FALSE);
+
